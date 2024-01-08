c7cd71766faa856f6f04321758dc4662d3f9cf12
anupa47 <124699623+anupa47@users.noreply.github.com>
2024-01-08 15:42:02 +0530

    add style

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-//let student = 20;
-//student = student+10;
+// //let student = 20;
+// //student = student+10;
 
-//console.log(student)
+// //console.log(student)
 
-//let username = window.prompt("what's your name")
-//console.log(username);
+// //let username = window.prompt("what's your name")
+// //console.log(username);
+
+// // let username;
+
+// // document.getElementById("button1").onclick = function(){
+// //     username = document.getElementById("text1").value;
+// //     console.log(username);
+// //     document.getElementById("username").textContent=  `${username}`;
+// //     document.getElementById("wel").textContent = `Hello ${username}`;
+// // }
 
 // let username;
 
-// document.getElementById("button1").onclick = function(){
+// document.getElementById("button1").onclick = function () {
 //     username = document.getElementById("text1").value;
 //     console.log(username);
-//     document.getElementById("username").textContent=  `${username}`;
-//     document.getElementById("wel").textContent = `Hello ${username}`;
+//     document.getElementById("wel").textContent = "usernam"
 // }
 
-let username;
 
-document.getElementById("button1").onclick = function () {
-    username = document.getElementById("text1").value;
-    console.log(username);
-    document.getElementById("wel").textContent = "usernam"
-}
+// // let cname;
 
+// // cname = window.prompt('what is your name?');
 
-// let cname;
+// // console.log(cname)
 
-// cname = window.prompt('what is your name?');
+// const pi = 3.14;
 
-// console.log(cname)
+// document.getElementById("submit").onclick = function () {
+//     let r = document.getElementById("radius").value;
+//     r = Number(r) 
+//     console.log(r, typeof r)
+//     let pp = 2 * pi * r;
 
-const pi = 3.14;
-
-document.getElementById("submit").onclick = function () {
-    let r = document.getElementById("radius").value;
-    r = Number(r) 
-    console.log(r, typeof r)
-    let pp = 2 * pi * r;
-
-    document.getElementById("perimeter").textContent = `Perimeter is ${pp}`;
-    console.log(pp)
-}
+//     document.getElementById("perimeter").textContent = `Perimeter is ${pp}`;
+//     console.log(pp)
+// }
